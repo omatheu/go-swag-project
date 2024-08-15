740b55646767eec0b3fdd506ee84f7b3d573b651
Matheus Ribeiro <matheus.ribeiro@sou.inteli.edu.br>
2024-08-15 15:43:20 -0300

    add: files of project

---
+++ fibersimple/main.go
@@ -0,0 +1,62 @@
+package main
+import (
+   "github.com/gofiber/fiber/v2/middleware/recover"
+   "github.com/gofiber/fiber/v2/middleware/cors"
+   "github.com/gofiber/fiber/v2"
+   "log"
+   _ "github.com/omatheu/go-swag-project/docs/fibersimple" // you need to update github.com/rizalgowandy/go-swag-sample with your own project path
+   swagger "github.com/arsmn/fiber-swagger/v2"
+)
+
+// @title Fiber Swagger Example API
+// @version 2.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /
+// @schemes http
+func main() {
+   // Fiber instance
+   app := fiber.New()
+
+   // Middleware
+   app.Use(recover.New())
+   app.Use(cors.New())
+
+   // Routes
+   app.Get("/", HealthCheck)
+   app.Get("/swagger/*", swagger.Handler) // default
+
+   // Start Server
+   if err := app.Listen(":3000"); err != nil {
+      log.Fatal(err)
+   }
+}
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func HealthCheck(c *fiber.Ctx) error {
+   res := map[string]interface{}{
+      "data": "Server is up and running",
+   }
+
+   if err := c.JSON(res); err != nil {
+      return err
+   }
+
+   return nil
+}
