c85d7b492e4a817dfc3e94456c8cb58caf298294
Matheus Ribeiro <matheus.ribeiro@sou.inteli.edu.br>
2024-08-15 23:23:10 -0300

    fix: swagger

--- fibersimple/main.go
+++ fibersimple/main.go
@@ -1,11 +1,12 @@
 package main
+
 import (
-   "github.com/gofiber/fiber/v2/middleware/recover"
-   "github.com/gofiber/fiber/v2/middleware/cors"
-   "github.com/gofiber/fiber/v2"
-   "log"
-   _ "github.com/omatheu/go-swag-project/docs/fibersimple" // you need to update github.com/rizalgowandy/go-swag-sample with your own project path
-   swagger "github.com/arsmn/fiber-swagger/v2"
+	swagger "github.com/arsmn/fiber-swagger/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	_ "github.com/omatheu/go-swag-project/docs/fibersimple" // you need to update github.com/rizalgowandy/go-swag-sample with your own project path
+	"log"
 )
 
 // @title Fiber Swagger Example API
@@ -24,21 +25,25 @@ import (
 // @BasePath /
 // @schemes http
 func main() {
-   // Fiber instance
-   app := fiber.New()
+	// Fiber instance
+	app := fiber.New()
 
-   // Middleware
-   app.Use(recover.New())
-   app.Use(cors.New())
+	// Middleware
+	app.Use(recover.New())
+	app.Use(cors.New())
 
-   // Routes
-   app.Get("/", HealthCheck)
-   app.Get("/swagger/*", swagger.Handler) // default
+	// Routes
+	app.Get("/", HealthCheck)
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
 
-   // Start Server
-   if err := app.Listen(":3000"); err != nil {
-      log.Fatal(err)
-   }
+	// New Routes
+	app.Get("/users", GetUsers)    // Get list of users
+	app.Post("/users", CreateUser) // Create a new user
+
+	// Start Server
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // HealthCheck godoc
@@ -50,13 +55,46 @@ func main() {
 // @Success 200 {object} map[string]interface{}
 // @Router / [get]
 func HealthCheck(c *fiber.Ctx) error {
-   res := map[string]interface{}{
-      "data": "Server is up and running",
-   }
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
 
-   if err := c.JSON(res); err != nil {
-      return err
-   }
+	if err := c.JSON(res); err != nil {
+		return err
+	}
 
-   return nil
+	return nil
+}
+
+// GetUsers godoc
+// @Summary Get list of users
+// @Description Get a list of users
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} map[string]interface{}
+// @Router /users [get]
+func GetUsers(c *fiber.Ctx) error {
+	users := []map[string]interface{}{
+		{"id": 1, "name": "John Doe"},
+		{"id": 2, "name": "Jane Doe"},
+	}
+	return c.JSON(users)
+}
+
+// CreateUser godoc
+// @Summary Create a new user
+// @Description Create a new user with the input payload
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param user body map[string]interface{} true "User"
+// @Success 201 {object} map[string]interface{}
+// @Router /users [post]
+func CreateUser(c *fiber.Ctx) error {
+	user := new(map[string]interface{})
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+	return c.Status(fiber.StatusCreated).JSON(user)
 }
--- go.mod
+++ go.mod
@@ -1,19 +1,20 @@
-module example.com/m/v2
+module github.com/omatheu/go-swag-project
 
 go 1.22.1
 
 require (
+	github.com/arsmn/fiber-swagger/v2 v2.31.1
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/swaggo/swag v1.16.3
+)
+
+require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/PuerkitoBio/purell v1.2.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/arsmn/fiber-swagger/v2 v2.31.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
@@ -22,19 +23,12 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
-	github.com/swaggo/swag v1.16.3 // indirect
-	github.com/urfave/cli/v2 v2.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
